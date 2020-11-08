062fda2981ef027c7b323d90b9214ca21a40a04d
Veeramuthur <46166592+Veeramuthur@users.noreply.github.com>
2020-11-08 21:17:36 +0530

    Add files via upload

---
+++ C2_simplesum_twonos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCSharpConsoleApp
+{
+    class C2_simplesum_twonos
+    {
+        static void Main(string[] args)
+        {
+            int a = 10; // a stord 10 values
+            int b = 20; // b stored 20 values
+            int c = a + b; // c stored 20 values
+            Console.WriteLine("Sum of values :" + c);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
