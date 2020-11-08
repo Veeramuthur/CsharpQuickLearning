2cdb17e921e051028cc08870788ebd936a99aede
Veeramuthur <46166592+Veeramuthur@users.noreply.github.com>
2020-11-08 20:58:03 +0530

    Add files via upload
    Simple firstname and lastname displayed in console window

---
+++ FirstHello.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCSharpConsoleApp
+{
+    class FirstHello
+    {
+        static void Main(string[] args)
+        {
+            string firstname = "veera";
+            string lastname = "muthu";
+            string fullname = firstname + " " + lastname;
+            Console.WriteLine("Hello: "+ fullname);
+            Console.ReadLine();
+        }
+    }
+}
