3b7da551311faace27dd83bb7564302c55023b9b
Veeramuthur <46166592+Veeramuthur@users.noreply.github.com>
2020-11-09 08:28:08 +0530

    Add files via upload

---
+++ C3_swaptwonumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCSharpConsoleApp
+{
+    class C3_swaptwonumbers
+    {
+        static void Main(string[] args)
+        {
+            int firstnumber = 7;
+            int secondnumber = 2;
+            int afterswap;
+
+            afterswap = firstnumber;
+            firstnumber = secondnumber;
+            secondnumber = afterswap;
+            Console.WriteLine("After swap:");
+            Console.WriteLine("FirstNumber " + firstnumber);
+            Console.WriteLine("SecondNumber " + secondnumber);
+
+            Console.ReadLine();
+
+        }
+    }
+}
