e0dc2b7ca73c49b5f83d8b3da177bee0f0989a9b
Veeramuthur <46166592+Veeramuthur@users.noreply.github.com>
2020-11-10 09:04:04 +0530

    Add files via upload

---
+++ C4_ReverseNnumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCSharpConsoleApp
+{
+    class C4_ReverseNnumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int Number = int.Parse(Console.ReadLine());
+            int reverse = 0;
+            while (Number>0)
+            {
+                int Remainder = Number % 10;
+                reverse = (reverse * 10) + Remainder;
+                Number = Number / 10;
+            }
+            Console.WriteLine("Reverse No. is {0}",reverse);
+            Console.ReadLine();
+        }
+    }
+}
